f6cbfecbfd78216610392045730c12c4316bc8b2
gxyqn626 <941146717@qq.com>
2019-08-09 23:51:04 +0800

    用户登陆注册修改密码

--- gxy/sell/src/main.js
+++ gxy/sell/src/main.js
@@ -4,18 +4,26 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
+import Vant from 'vant'
+import store from './store/store'
+import axios from 'axios'
+import 'vant/lib/index.css'
 import 'lib-flexible/flexible'
 
 import './common/stylus/index.styl'
+import './assets/iconfont/iconfont.css'
 
+Vue.prototype.$axios = axios;
 Vue.use(VueResource)
-
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
+  axios,
   components: { App },
   template: '<App/>'
 })
