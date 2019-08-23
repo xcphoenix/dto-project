23a7ae84f3158ce0f04b4b5aa58659359cfda6fb
xcphoenix <root@xcphoenix.top>
2019-08-23 20:14:30 +0800

    ...

---
+++ back-end/Dto/src/main/java/com/xcphoenix/dto/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.xcphoenix.dto.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author      xuanc
+ * @date        2019/8/23 下午6:50
+ * @version     1.0
+ */
+@RestController
+public class UserController {
+
+}
--- back-end/Dto/src/test/java/com/xcphoenix/dto/DtoApplicationTests.java
+++ back-end/Dto/src/test/java/com/xcphoenix/dto/DtoApplicationTests.java
@@ -1,8 +1,5 @@
 package com.xcphoenix.dto;
 
-import com.alibaba.fastjson.JSON;
-import com.xcphoenix.dto.bean.Restaurant;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,13 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DtoApplicationTests {
-
-    @Test
-    public void tmpTest() {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setRestaurantDesc("");
-        restaurant.setRestaurantName("test");
-        System.out.println(JSON.toJSON(restaurant));
-    }
 
 }
