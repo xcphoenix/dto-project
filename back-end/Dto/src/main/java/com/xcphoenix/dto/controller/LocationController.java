6006634aeda79586e7b6845d1b4cf5af3a7070b5
xcphoenix <root@xcphoenix.top>
2020-02-26 18:22:20 +0800

    fix(location): 恢复ip定位

--- back-end/Dto/src/main/java/com/xcphoenix/dto/controller/LocationController.java
+++ back-end/Dto/src/main/java/com/xcphoenix/dto/controller/LocationController.java
@@ -2,12 +2,15 @@ package com.xcphoenix.dto.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.xcphoenix.dto.annotation.PassToken;
+import com.xcphoenix.dto.result.ErrorCode;
 import com.xcphoenix.dto.result.Result;
 import com.xcphoenix.dto.service.GeoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author      xuanc
@@ -26,24 +29,14 @@ public class LocationController {
 
     @PassToken
     @GetMapping("/ip")
-    public JSONObject location() {
-        // JSONObject jsonObject = geoService.getLocation();
-        //
-        // if (jsonObject != null) {
-        //     Map<String, Object> resultMap = jsonObject.getInnerMap();
-        //     return new Result(null, resultMap);
-        // }
-        // return Result.error(ErrorCode.LOCATION_FAILED);
-        return JSONObject.parseObject("{\n" +
-                "\t\"data\":{\n" +
-                "\t\t\"address\":\"xxxxx\",\n" +
-                "\t\t\"location\":{\n" +
-                "\t\t\t\"lng\":108.874,\n" +
-                "\t\t\t\"lat\":34.159\n" +
-                "\t\t}\n" +
-                "\t},\n" +
-                "\t\"status\":200\n" +
-                "}");
+    public Result location() {
+        JSONObject jsonObject = geoService.getLocation();
+
+        if (jsonObject != null) {
+            Map<String, Object> resultMap = jsonObject.getInnerMap();
+            return new Result(null, resultMap);
+        }
+        return Result.error(ErrorCode.LOCATION_FAILED);
     }
 
     @PassToken
